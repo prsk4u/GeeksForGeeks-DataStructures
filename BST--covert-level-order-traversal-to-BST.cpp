251f13f4b9865ba7c6b9328bb147dcac9601d524
prsk4u <39626103+prsk4u@users.noreply.github.com>
2020-07-01 19:09:06 +0530

    Create BST--covert-level-order-traversal-to-BST.cpp

---
+++ BST--covert-level-order-traversal-to-BST.cpp
@@ -0,0 +1,72 @@
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+void preorderTraversal(Node* root)
+{
+	if (!root)return;
+	cout << root->data << " ";
+	preorderTraversal(root->left);
+	preorderTraversal(root->right); 
+}
+
+Node* constructBst(int arr[], int n);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)cin>>arr[i];
+        Node *root = constructBst(arr, n);
+        preorderTraversal(root);
+        cout<<endl;
+    }
+	return 0; 
+}// } Driver Code Ends
+
+//User function Template for C++
+/*
+Structure of the Node of the node of the bst 
+should be as
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+*/
+// You are required to complete this function
+Node* insert(Node* root,int key){
+    if(root==NULL)return new Node(key);
+    if(root->data>key) root->left=insert(root->left,key);
+    else root->right=insert(root->right,key);
+    return root;
+}
+Node* constructBst(int arr[], int n)
+{
+    Node *root=NULL;
+    for(int i=0;i<n;i++) root=insert(root,arr[i]);
+    return root;
+}
